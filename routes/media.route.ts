86d07d165e166e5ca5751e7a32ab945a94d839da
Ansh Kumar <anshkumar8710@gmail.com>
2025-10-15 11:23:54 +0530

    Fix media route

--- routes/media.route.ts
+++ routes/media.route.ts
@@ -1,22 +1,9 @@
 import express from 'express';
-import { uploadMedia } from '@utils/cloudinary';
 import upload from '@/middlewares/multer.middleware';
+import { uploadVideo } from '@/controllers/media.controller';
 
 const router = express.Router();
 
-router
-  .route('/upload-video')
-  .post(upload.single('file'), async (request, response) => {
-    try {
-      const result = await uploadMedia(request?.file?.path!);
-      response.status(200).json({
-        success: true,
-        message: 'File uploaded successfully.',
-        data: result
-      });
-    } catch (error) {
-      console.log(error);
-      response.status(500).json({ message: 'Error uploading file' });
-    }
-  });
+router.route('/upload-video').post(upload.single('file'), uploadVideo);
+
 export default router;
